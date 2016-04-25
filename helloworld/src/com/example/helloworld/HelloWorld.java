b54d8f785496c340fb0352e870570831f554e7d1
Aemon <aemon_delosreyes@yahoo.com>
2016-04-25 09:32:34 +0800

    upload code

---
+++ helloworld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by Aemon on 4/16/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!"); 
+    }
+}
